d98340e068ef9f2512c7686611eb293ca018e0c7
Михаил <89040307970@mail.ru>
2017-12-08 17:13:16 +0300

    привет

---
+++ MashZavod/Models/NewsModel.cs
@@ -0,0 +1,16 @@
+﻿namespace MashZavod.Models
+{
+    public class NewsModel
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+        public string PubDate { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+
+        public NewsModel()
+        {
+
+        }
+    }
+}
