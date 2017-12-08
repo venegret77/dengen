d6c3e12b2bb4bfaa6241fdb5defccb9fabbc538e
Vladimir <vldr.lush@gmail.com>
2017-12-08 20:00:03 +0300

    я так и не понял, как эти хидены делать

---
+++ MashZavod/Controllers/BigChatController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MashZavod.Models.DbModels;
+using MashZavod.Models;
+
+namespace MashZavod.Controllers
+{
+    public class BigChatController : Controller
+    {
+        // GET: BigChat
+        [HttpGet]
+        public ActionResult Index()
+        {
+            if (!User.Identity.IsAuthenticated)
+                return RedirectToAction("Login", "Account");
+
+            List<BigChatModels> ListMessage = new List<BigChatModels>();
+            /*Возвращает историю сообщений*/
+            using (database_murom_factory2Entities1 db = new database_murom_factory2Entities1())
+            {
+                var zaprosik = db.message;
+
+
+                foreach (var el in zaprosik)
+                {
+                    ListMessage.Add(new BigChatModels { Login = el.id_users.ToString(), Message = el.text_message, Time = el.datetime });
+                }//Login = db.users.Where(u => u.id_users == el.LoginUser)
+            }
+            ViewBag.Chat = ListMessage;
+
+
+            return PartialView("BigChat");
+        }
+
+        [HttpPost]
+        public void Index(message model)
+        {
+            using (database_murom_factory2Entities1 db = new database_murom_factory2Entities1())
+            {
+                db.message.Add(model);
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ MashZavod/Models/BigChatModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MashZavod.Models
+{
+    public class BigChatModels
+    {
+        public string Login { get; set; }
+        public Nullable<DateTime> Time { get; set; }
+        public string Message { get; set; }
+    }
+}
