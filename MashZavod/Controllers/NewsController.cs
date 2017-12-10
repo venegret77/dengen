fd1b7bb2e0d6132240f920700836ea042a328c1e
Михаил <89040307970@mail.ru>
2017-12-10 11:21:57 +0300

    1

--- MashZavod/Controllers/NewsController.cs
+++ MashZavod/Controllers/NewsController.cs
@@ -17,9 +17,9 @@ namespace MashZavod.Controllers
 
         public ActionResult GetNews()
         {
-            XDocument feedXML = XDocument.Load("https://news.rambler.ru/rss/tech/");
-
-            var feeds = from feed in feedXML.Descendants("item")
+            //Технологии
+            XDocument TechnXML = XDocument.Load("https://news.yandex.ru/computers.rss");
+            var Techn = from feed in TechnXML.Descendants("item")
                         select new
                         {
                             Title = feed.Element("title").Value,
@@ -27,6 +27,38 @@ namespace MashZavod.Controllers
                             Description = feed.Element("description").Value,
                             PubDate = feed.Element("pubDate").Value
                         };
+            //Наука
+            XDocument ScienceXML = XDocument.Load("https://news.yandex.ru/science.rss");
+            var Science = from feed in ScienceXML.Descendants("item")
+                        select new
+                        {
+                            Title = feed.Element("title").Value,
+                            Link = feed.Element("link").Value,
+                            Description = feed.Element("description").Value,
+                            PubDate = feed.Element("pubDate").Value
+                        };
+            //Владимирская область
+            XDocument VladimirXML = XDocument.Load("https://news.yandex.ru/Vladimir/index.rss");
+            var Vladimir = from feed in VladimirXML.Descendants("item")
+                        select new
+                        {
+                            Title = feed.Element("title").Value,
+                            Link = feed.Element("link").Value,
+                            Description = feed.Element("description").Value,
+                            PubDate = feed.Element("pubDate").Value
+                        };
+            //Политика
+            XDocument PoliticXML = XDocument.Load("https://news.yandex.ru/politics.rss");
+            var Politic = from feed in PoliticXML.Descendants("item")
+                           select new
+                           {
+                               Title = feed.Element("title").Value,
+                               Link = feed.Element("link").Value,
+                               Description = feed.Element("description").Value,
+                               PubDate = feed.Element("pubDate").Value
+                           };
+            //Заносим все в единый массив
+            //Возвращаем
             return null;
         }
     }
