eca2fca1ed1de92f623b59b32eb87bbd49dc0c9c
Vladimir <vldr.lush@gmail.com>
2017-12-19 18:01:19 +0300

    володья

--- MashZavod/Models/BigChatModels.cs
+++ MashZavod/Models/BigChatModels.cs
@@ -2,13 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace MashZavod.Models
 {
     public class BigChatModels
     {
+        [Required]
         public string Login { get; set; }//логин
-        public Nullable<DateTime> Time { get; set; }//дата
-        public string Message { get; set; }//сообщение
+
+        [Required]
+        public DateTime Time_message;//дата отправки сообщения(должна быть точность до милисекунд)
+
+        [Required]
+        public string Text_message { get; set; }//текст сообщения
+
+        [Required]
+        public int Type_chat { get; set; }//тип чата
+
+        [Required]
+        public string Name_chat { get; set; }//название чата
+
+        [Required]
+        public DateTime Time_create { get; set; }//дата создания чата
+
     }
 }
